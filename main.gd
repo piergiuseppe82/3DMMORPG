a358a34f6ed4bf05af8fa66deec5a5b048d13580
Piergiuseppe La Cava <piergiuseppe82@gmail.com>
2024-04-28 08:57:01 +0200

    Dynamic map load

--- main.gd
+++ main.gd
@@ -1,10 +1,13 @@
 extends Node
 
 var player_pre = preload("res://characters/mc_player.tscn")
+var level_pre = preload("res://world/region_001.tscn")
 
 func _on_multiplayer_ui_connected(id):
+	var map = load_map(id)
+	call_deferred("add_child",map)
 	var player = load_player(id)
-	call_deferred("add_child",player)
+	map.call_deferred("add_child",player)
 	
 	
 
@@ -13,3 +16,8 @@ func load_player(id):
 	var player = player_pre.instantiate()
 	player.name = str(id)
 	return player
+
+func load_map(id):
+	var map = level_pre.instantiate()
+	map.name = str(id)
+	return map
